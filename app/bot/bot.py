eb6035c0d9f26b1b1d9bdfd7beeed9eb1784c77a
陈序员 <138159016+dunea@users.noreply.github.com>
2025-02-18 12:05:25 +0800

    🔄 refactor(bot): 重命名 bot_commands, bot_messages 到 bot_command, bot_message

--- app/bot/bot.py
+++ app/bot/bot.py
@@ -2,8 +2,7 @@ import logging
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters
 
-from app.bot import bot_commands, bot_messages
-from app.core import settings
+from app.bot import bot_command, bot_message
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -14,16 +13,16 @@ logging.basicConfig(
 class Bot:
     def __init__(self, token: str):
         self.application = ApplicationBuilder().token(token).build()
-        self.commands()
-        self.messages()
+        self.add_command_group()
+        self.add_message_group()
     
     def run_polling(self):
         self.application.run_polling()
     
-    def commands(self):
-        start_handler = CommandHandler('start', bot_commands.start)
+    def add_command_group(self):
+        start_handler = CommandHandler('start', bot_command.start)
         self.application.add_handler(start_handler)
     
-    def messages(self):
-        echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), bot_messages.echo)
+    def add_message_group(self):
+        echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), bot_message.echo)
         self.application.add_handler(echo_handler)
