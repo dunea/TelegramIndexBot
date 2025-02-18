92c8012ad37db0e4dbe59db093614b1eaf10720e
陈序员 <138159016+dunea@users.noreply.github.com>
2025-02-18 11:47:23 +0800

    :art: 改进bot消息的代码结构

--- app/bot/bot.py
+++ app/bot/bot.py
@@ -2,17 +2,13 @@ import logging
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters
 
-from app.bot import bot_commands
+from app.bot import bot_commands, bot_messages
 from app.core import settings
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
-
-
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
 
 
 class Bot:
@@ -29,5 +25,5 @@ class Bot:
         self.application.add_handler(start_handler)
     
     def messages(self):
-        echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+        echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), bot_messages.echo)
         self.application.add_handler(echo_handler)
